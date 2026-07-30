a664caa784fd09204aa4a49d39d0308be750957c
Chitranshkumar18 <chitranshkumar730@gmail.com>
2026-07-30 17:52:34 +0530

    arr

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -7,7 +7,7 @@ const myHero = ["virat" ,"shaktiman"];
 //Array check karne ka correct way => (Array.isArray(myHero))
 
 const myArr2 = new Array(1,2,3,4,6);
-// console.log(typeof myArr );// object =>  JavaScript me Array bhi object hota hai.isliye typeog se object or array mai diffrence ni pta chalta hai
+// console.log(typeof myArr );// object =>  JavaScript me Array bhi object hota hai.isliye typeof se object or array mai diffrence ni pta chalta hai
 // console.log(typeof myHero);// object
 
 
